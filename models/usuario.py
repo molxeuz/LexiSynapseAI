3d5de9714e2e81dd15f059ba08ddb5a135ed7879
Moises Joshua <moisesjoshua53@gmail.com>
2025-03-09 08:14:35 -0500

    Update usuario.py
    add class usuario

---
+++ models/usuario.py
@@ -0,0 +1,12 @@
+class Usuario:
+    #Atributos
+    def __init__(self,nombre: str):
+        self.nombre: str= nombre
+        self.preferencias: list[str]=[]
+        self.historial: list[str]=[]
+
+    #Metodos
+    def mostrar_usuario(self):
+        return f"{self.nombre}-{self.preferencias}-{self.historial}"
+
+
