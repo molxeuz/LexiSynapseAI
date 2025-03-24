aa2e36fa1dbd34ab4aa70329cde7afd344d74ff3
TheNameAndresWasTaken <andresdavidagudelo01@gmail.com>
2025-03-24 18:07:51 -0500

    Added @classmethod
    Re definido "devolver" a "convertir_a_texto", organizacion y implementacion del @classmethod (1)
    añadido el return cls y except

--- controllers/tarea_controller.py
+++ controllers/tarea_controller.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Tarea:
     def __int__(self, descripcion, fecha_limite, completada=False):
         self.descripcion = descripcion
@@ -8,9 +11,24 @@ class Tarea:
         estado = "v" if self.completada else " "
         return f"[{estado}] {self.descripcion} (Para: {self.fecha_limite})"
 
-    def devolver(self):
-        return f"TAREA, {self.descripcion},{self.fecha_limite}, {int(self.completada)}"
+    def convertir_a_texto(self):
+        return f"TAREA,{self.descripcion},{self.fecha_limite},{int(self.completada)}"
 
+    @classmethod
+    def crear_desde_texto(cls, texto):
+        try:
+            partes = texto.strip().split(',')
+            if len(partes) != 4 or partes[0] != "TAREA":
+                raise ValueError("Formato incorrecto")
+
+            return cls(
+                descripcion=partes[1],
+                fecha_limite=datetime.date.fromisoformat(partes[2]),
+                completada=bool(int(partes[3]))
+            )
+        except Exception as error:
+            print(f"Error al crear tarea: {error}")
+            return None
 """
 CRUD tareas.
 
