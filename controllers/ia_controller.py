ca7c988dc72e1f8c5fd9bd4ad0713f647761af1a
Moises Joshua <moisesjoshua53@gmail.com>
2025-04-07 21:56:06 -0500

    add/class_Calendar

--- controllers/ia_controller.py
+++ controllers/ia_controller.py
@@ -1,3 +1,13 @@
+class AsistenteIA:
+    def __init__(self, id_usuario: int):
+        self.id_usuario: int = id_usuario
+
+    #Metodos
+    def consultar_ia(self, consulta: str) -> None:
+        pass
+
+    def sugerencias_estudio(self, id_usuario: int) -> None:
+        pass
 
 """
 Consultas IA.
